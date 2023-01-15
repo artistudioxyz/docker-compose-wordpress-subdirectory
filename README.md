6a1905a6e8d7c0e16d5b4fd2ee7ec2814dbce0a3
Agung Sundoro <muhagung2001@gmail.com>
2023-01-16 06:54:26 +0700

    chore: initial commit add directories and files

---
+++ README.md
@@ -0,0 +1,11 @@
+# 🐳 Docker Compose WordPress in Subdirectory
+
+## 📝 Installation
+- Copy file `cp .env-sample .env`
+- Change `.env` details
+- Run container `docker-compose up -d`
+
+## ⭐️ Support & Contribution
+- Help support me by giving a 🌟 or [donate][website]
+
+[website]: https://agung2001.github.io
---
+++ wp/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress</title>
+</head>
+<body>
+<img src="https://s.w.org/style/images/about/WordPress-logotype-standard.png" alt="Wordpress">
+</body>
+</html>
